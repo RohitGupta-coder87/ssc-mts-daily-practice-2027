befb9fefa92d7e368fe19a58247344c1031fbb1c
RohitGupta <r7549053373@gmail.com>
2026-09-03 10:39:12 +0530

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SSC MTS Daily Practice 2027"
+include(":app")
